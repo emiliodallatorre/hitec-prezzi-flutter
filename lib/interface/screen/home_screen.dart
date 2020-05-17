216245b61897c25c2d878209c7410d6533a486a1
Emilio Dalla Torre <emiliodallatorre12@live.com>
2020-05-17 11:50:33 +0200

    Sistemata la struttura del progetto.

---
+++ lib/interface/screen/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String route = "/homeScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Creatore etichette Prezzi - Hi-Tec"),),
+    );
+  }
+}
