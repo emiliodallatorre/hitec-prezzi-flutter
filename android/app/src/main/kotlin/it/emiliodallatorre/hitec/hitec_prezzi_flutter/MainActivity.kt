e1a647aec33ae3d1b8eb66ab0c0ea367a693580b
Emilio Dalla Torre <emiliodallatorre12@live.com>
2020-05-17 15:48:05 +0200

    Iniziato il lavoro sulla homescreen.
    Took 18 minutes

---
+++ android/app/src/main/kotlin/it/emiliodallatorre/hitec/hitec_prezzi_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package it.emiliodallatorre.hitec.hitec_prezzi_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/interface/screen/home_screen.dart
+++ lib/interface/screen/home_screen.dart
@@ -1,12 +1,81 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String route = "/homeScreen";
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController _textController;
+
+  @override
+  void initState() {
+    _textController = TextEditingController();
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Creatore etichette Prezzi - Hi-Tec"),),
+      appBar: AppBar(
+        title: Text("Creatore etichette Prezzi - Hi-Tec"),
+      ),
+      body: _buildBody(context),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: AspectRatio(
+                  aspectRatio: 1 / sqrt2,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ),
+              Text("Preview", style: Theme.of(context).textTheme.caption),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8.0, right: 16.0),
+            child: ListView(
+              children: [
+                Row(
+                  children: [
+                    Expanded(child: TextField(
+                      controller: _textController,
+                    )),
+                    VerticalDivider(color: Colors.transparent),
+                    RaisedButton(
+                      child: Text("Aggiungi"),
+                      onPressed: () {
+                        debugPrint("Aggiunto un elemento: ${_textController.text}.");
+                        _textController.clear();
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
