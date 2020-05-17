81ab813efbe619d69672b96ae080812aefedf6f0
Emilio Dalla Torre <emiliodallatorre12@live.com>
2020-05-17 18:43:28 +0200

    Modificato il salvataggio, forse inutile.

--- lib/interface/screen/home_screen.dart
+++ lib/interface/screen/home_screen.dart
@@ -66,7 +66,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: PdfPreview(
                     initialPageFormat: PdfPageFormat.a4,
                     canChangePageFormat: false,
-                    build: (PdfPageFormat format) => pdfDocumentToUint8List(pdfDocument),
+                    build: (PdfPageFormat format) => pdfDocument.save(),
                   ),
                 ),
               ),
